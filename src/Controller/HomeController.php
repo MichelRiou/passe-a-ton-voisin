8c2de780f27d4c69bacc3afde6144a1d62612b52
Michel RIOU <michel.riou@laposte.net>
2018-03-02 13:04:00 +0100

    Mise en place service monolog.logger voir HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,7 +19,9 @@ class HomeController extends Controller
      * @Route("/",name="homepage")
      * @return Response
      */
-            public function indexAction(){
+            public function indexAction(LoggerInterface $logger){
+            // Test service de Log Monolog //
+                $logger->info("Je suis passé par là",[self::class]);
 
                 //return new Response("Holà");
                 return $this->render("home/index.html.twig");
